b3335a6dff61f13832630f813fffccf209b19c57
Robert Cabrera <robertcabrera4871@gmail.com>
2020-02-14 13:15:30 -0500

    added base requirement functionality, needs '.' error check and bug when Edit

---
+++ app/src/main/java/com/example/currencycalculator/CurrencyCalculator.kt
@@ -0,0 +1,61 @@
+package com.example.currencycalculator
+
+import android.icu.util.CurrencyAmount
+
+
+class CurrencyCalculator {
+    /*
+    <item>Mex Peso</item>
+    <item>US Dollar</item>
+    <item>Can Dollar</item>
+    <item>Jap Yen</item>*/
+
+    var fromCurrencyAmount: Double = 0.0
+     var fromCurrencyType : String = ""
+     var toCurrencyType: String = ""
+
+    fun calculateToCurrency() : String{
+        if( fromCurrencyType!= "" && toCurrencyType != ""){
+            when(fromCurrencyType){
+                "Mex Peso" -> return fromMexCalc().toString().plus(" Pesos")
+                "Can Dollar" -> return fromCanCalc().toString().plus(" Can")
+                "Jap Yen" -> return fromJapCalc().toString().plus(" Yen")
+                "US Dollar" -> return fromUsCalc().toString().plus(" US")
+            }
+        }
+        return ""
+    }
+    private fun fromUsCalc(): Double{
+            when(toCurrencyType){
+            "Mex Peso" -> return fromCurrencyAmount * 18.56
+            "Can Dollar" -> return fromCurrencyAmount * 1.33
+            "Jap Yen" -> return fromCurrencyAmount * 109.74
+        }
+        return fromCurrencyAmount
+    }
+    private fun fromMexCalc(): Double{
+        when(toCurrencyType){
+            "US Dollar" -> return fromCurrencyAmount * 0.054
+            "Can Dollar" -> return fromCurrencyAmount * 0.071
+            "Jap Yen" -> return fromCurrencyAmount * 5.92
+        }
+        return fromCurrencyAmount
+    }
+    private fun fromCanCalc(): Double{
+        when(toCurrencyType){
+            "Mex Peso" -> return fromCurrencyAmount * 13.99
+            "US Dollar" -> return fromCurrencyAmount * 0.75
+            "Jap Yen" -> return fromCurrencyAmount * 82.80
+        }
+        return fromCurrencyAmount
+    }
+    private fun fromJapCalc(): Double{
+        when(toCurrencyType) {
+            "Mex Peso" -> return fromCurrencyAmount * 0.17
+            "US Dollar" -> return fromCurrencyAmount * 0.0091
+            "Can Dollar" -> return fromCurrencyAmount * 0.012
+        }
+        return fromCurrencyAmount
+    }
+
+}
